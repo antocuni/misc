0139c6f4827f88c2a87d441817dc1c0a5a276891
Antonio Cuni <anto.cuni@gmail.com>
2012-01-17 17:22:46 +0100

    small script to dump a python parse tree with symbolic names

---
+++ hack/dump_parse_tree.py
@@ -0,0 +1,21 @@
+import parser
+import symbol
+import pprint
+
+def parse_and_dump(src):
+    st = parser.suite(src)
+    lst = st.tolist()
+    lst = putsym(lst)
+    pprint.pprint(lst)
+
+def putsym(lst):
+    res = []
+    for item in lst:
+        if isinstance(item, int):
+            item = symbol.sym_name.get(item, item)
+        elif isinstance(item, list):
+            item = putsym(item)
+        res.append(item)
+    return res
+
+parse_and_dump(sys.argv[1])
