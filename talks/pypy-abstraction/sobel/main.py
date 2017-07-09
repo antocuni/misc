0aa91b2a26acb10fa9b8791db598a079f466cc17
Antonio Cuni <anto.cuni@gmail.com>
2017-07-09 18:23:18 +0200

    write an even lower level; move v0 and v1 to their own file

--- talks/pypy-abstraction/sobel/main.py
+++ talks/pypy-abstraction/sobel/main.py
@@ -8,41 +8,6 @@ from image import Image
 from math import sqrt
 import array
 
-def get(img, x, y):
-    w, h, data = img
-    i = x + y*w
-    return data[i]
-
-def set(img, x, y, value):
-    w, h, data = img
-    i = x + y*w
-    data[i] = value
-
-
-def sobel_v0(img):
-    w, h, data = img
-    out = w, h, array.array('B', [0]) * (w*h)
-    for y in xrange(1, h-1):
-        for x in xrange(1, w-1):
-            dx = (-1.0 * get(img, x-1, y-1) +
-                   1.0 * get(img, x+1, y-1) +
-                  -2.0 * get(img, x-1, y)   +
-                   2.0 * get(img, x+1, y)   +
-                  -1.0 * get(img, x-1, y+1) +
-                   1.0 * get(img, x+1, y+1))
-            #
-            dy = (-1.0 * get(img, x-1, y-1) +
-                  -2.0 * get(img, x,   y-1) +
-                  -1.0 * get(img, x+1, y-1) +
-                   1.0 * get(img, x-1, y+1) +
-                   2.0 * get(img, x,   y+1) +
-                   1.0 * get(img, x+1, y+1))
-            #
-            value = min(int(sqrt(dx*dx + dy*dy) / 2.0), 255)
-            set(out, x, y, value)
-    return out
-
-
 def sobel_magnitude(img):
     res = img.clone()
     for p in img.pixeliter():
